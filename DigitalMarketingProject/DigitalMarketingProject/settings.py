f8797f7d0701a6c4a414cc143721c2025b401afa
vishalvu7 <vishalvu7@gmail.com>
2023-04-03 15:56:00 +0530

    third commit

--- DigitalMarketingProject/DigitalMarketingProject/settings.py
+++ DigitalMarketingProject/DigitalMarketingProject/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
-import dj_database_url
+# import django_heroku
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,6 +132,6 @@ LOGIN_URL = ''
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = (os.path.join(BASE_DIR,'static'),)
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 
